5de96c1f9de0eb102b70ff0bbbfd520da29690f4
Ryan <ryanjdayrit@intelimina.com>
2017-05-02 16:34:29 +0800

    Added minor changes to the layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   validates :username, uniqueness: { case_sensitive: false }, 
   presence: true, length: { minimum: 5, maximum: 50 }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 5 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   # get  '/help',    to: 'static_pages#help', as: 'helf'
   # get  '/about',   to: 'static_pages#about'
   # get  '/contact', to: 'static_pages#contact'
-  get  '/signup',  to: 'users#new'
-  post '/signup',  to: 'users#create'
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
+  get    '/signup',  to: 'users#new'
+  post   '/signup',  to: 'users#create'
+  get     '/login',  to: 'sessions#new'
+  post    '/login',  to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :users
 end
