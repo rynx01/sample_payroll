c4e10df0cd68775efe65748a2c05767a09b4ed01
Ryan <ryanjdayrit@intelimina.com>
2017-05-08 17:39:03 +0800

    Update stylings

--- db/migrate/20170504095057_create_reimbursements.rb
+++ db/migrate/20170504095057_create_reimbursements.rb
@@ -3,7 +3,7 @@ class CreateReimbursements < ActiveRecord::Migration[5.0]
     create_table :reimbursements do |t|
       t.string :category
       t.string :description
-      t.decimal :amount
+      t.decimal :amount, :price, :precision => 9, :scale => 2
       t.references :user, foreign_key: true
 
       t.timestamps
